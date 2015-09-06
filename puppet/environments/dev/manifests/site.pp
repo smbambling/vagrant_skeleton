8dbdda4b814996ef6c92afd3eaca7cf201323e81
Steven Bambling <smbambling@gmail.com>
2015-09-06 17:35:34 -0400

    adding comments

--- puppet/environments/dev/manifests/site.pp
+++ puppet/environments/dev/manifests/site.pp
@@ -1,7 +1,9 @@
 include stdlib
 
+# Install *.example.dev Wildcard PKI certificate
 $sslcerts = hiera(certs_for_system)
 create_resources(sslmgmt::cert, $sslcerts)
 
+# Install *.example.dev CA Certificate
 $cacerts = hiera(ca_certs_for_system)
 create_resources(sslmgmt::ca_dh, $cacerts)
